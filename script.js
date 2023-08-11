c259209bd30530fad28f5b14baf331aa7097db95
Jessica Beauchemin <123212907+JBeauche@users.noreply.github.com>
2023-08-11 15:14:38 -0400

    js function for events hide/slideshow

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-
-// Close side navigation
-function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
-}
-
 // Used to toggle the menu on smaller screens when clicking on the menu button
 function openNav() {
   var x = document.getElementById("navDemo");
   if (x.className.indexOf("show") == -1) {
     x.className += " show";
-  } else { 
+  } else {
     x.className = x.className.replace(" show", "");
   }
+}
+function getEvent(id) {
+  const eventid = id.toString();
+  console.log(id);
+  console.log(eventid);
+  document.getElementById(eventid).style.display = "block";
+  hideEvents(eventid);
 }
